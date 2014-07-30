8062db343ee4dd8dbe1062218117b45aa078eb6a
Nan Liu <nan.liu@gmail.com>
2014-07-29 23:23:54 -0700

    Add puppet enterprise installation.

--- script/cmtool.sh
+++ script/cmtool.sh
@@ -7,6 +7,7 @@
 #   'chef'            -- build a box with the Chef
 #   'chefdk'          -- build a box with the Chef Development Kit
 #   'puppet'          -- build a box with the Puppet
+#   'pe'              -- build a box with Puppet Enterprise
 #
 # Values for CM_VERSION can be (when CM is chef|salt|puppet):
 #   'x.y.z'           -- build a box with version x.y.z of Chef
@@ -83,6 +84,65 @@ install_puppet()
     fi
 }
 
+install_puppet_enterprise()
+{
+  echo "==> Installing Puppet Enterprise"
+
+  if [[ ${CM_VERSION:-} == 'latest' ]]; then
+    echo "==> Downloading latest Puppet Enterprise"
+    VERSION='3.2.3'
+  else
+    echo "==> Downloading Puppet Enterprise version ${CM_VERSION}"
+    VERSION=$CM_VERSION
+  fi
+
+  REDHAT_MAJOR_VERSION=$(egrep -Eo 'release ([0-9][0-9.]*)' /etc/redhat-release | cut -f2 -d' ' | cut -f1 -d.)
+  PKGNAME="puppet-enterprise-${VERSION}-el-${REDHAT_MAJOR_VERSION}-x86_64"
+  TARFILE="${PKGNAME}.tar.gz"
+  URL="https://pm.puppetlabs.com/puppet-enterprise/${VERSION}/${TARFILE}"
+
+  TMPDIR='/tmp'
+  HOSTNAME=`hostname`
+  cd $TMPDIR
+  echo "Fetching ${TARFILE}"
+  [ -e $TARFILE ] || curl -fLO $URL
+  echo "Extracting ${TARFILE}"
+  [ -d $PKGNAME ] || tar -xf $TARFILE
+  cd $PKGNAME
+
+  cat > agent.ans <<EOF
+q_all_in_one_install=n
+q_database_install=n
+q_pe_database=n
+q_puppetca_install=n
+q_puppetdb_install=n
+q_puppetmaster_install=n
+q_puppet_enterpriseconsole_install=n
+q_run_updtvpkg=n
+q_continue_or_reenter_master_hostname=c
+q_fail_on_unsuccessful_master_lookup=n
+q_puppetagent_certname=$HOSTNAME
+q_puppetagent_install=y
+q_puppetagent_server=puppet
+q_puppet_cloud_install=y
+q_puppet_symlinks_install=y
+q_vendor_packages_install=y
+q_install=y
+EOF
+
+  ./puppet-enterprise-installer -a agent.ans
+
+  # Cleanup
+  cd $TMPDIR && rm $TARFILE && rm -rf $PKGNAME
+
+  # Remove certname so the system will use host FQDN
+  sed -i '/certname =/d' /etc/puppetlabs/puppet/puppet.conf
+
+  # Symlink so puppet is in vagrant provisioner PATH
+  ln -s /opt/puppet/bin/facter /usr/bin/facter
+  ln -s /opt/puppet/bin/puppet /usr/bin/puppet
+}
+
 #
 # Main script
 #
@@ -102,6 +162,10 @@ case "${CM}" in
 
   'puppet')
     install_puppet
+    ;;
+
+  'pe')
+    install_puppet_enterprise
     ;;
 
   *)
