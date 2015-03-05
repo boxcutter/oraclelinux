64cdf67e9de4c2f350ba62de1239e688012fa35e
Moujan Anna <moujan@annawake.com>
2015-03-04 20:07:51 -0800

    Refactoring test code

--- test/oel_spec.rb
+++ test/oel_spec.rb
@@ -9,11 +9,10 @@ describe 'box' do
     expect(selinux).to be_disabled
   end
 
-  it 'should have single-request-reopen on virtualbox' do
-    vbox_string = command("dmesg | grep VirtualBox").stdout
-    if vbox_string.include? 'VirtualBox' then
-      expect(file('/etc/resolve.conf').content).to match /single-request-reopen/
-    end
+  vbox_string = command("dmesg | grep VirtualBox").stdout
+  has_vbox = vbox_string.include? 'VirtualBox'
+  it 'should have single-request-reopen on virtualbox', :if => has_vbox do
+    expect(file('/etc/resolv.conf').content).to match /single-request-reopen/
   end
 
   # https://www.chef.io/blog/2015/02/26/bento-box-update-for-centos-and-fedora/
@@ -21,5 +20,10 @@ describe 'box' do
     it 'uses the vendor-supplied openssl certificates' do
       expect(command('openssl s_client -CAfile /etc/pki/tls/certs/ca-bundle.crt -connect packagecloud-repositories.s3.amazonaws.com:443 </dev/null 2>&1 | grep -i "verify return code"').stdout).to match /\s+Verify return code: 0 \(ok\)/
     end
+  end
+
+  has_docker = command('command -v docker').exit_status == 0
+  it 'should make vagrant a member of the docker group', :if => has_docker do
+    expect(user 'vagrant').to belong_to_group 'docker'
   end
 end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -2,3 +2,6 @@ require 'serverspec'
 require 'net/ssh'
 
 set :backend, :ssh
+
+# Set PATH (OEL 5 does not include /sbin by default)
+set :path, '/sbin:/usr/local/sbin:$PATH'
