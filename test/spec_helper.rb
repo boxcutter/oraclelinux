b97158752677149785258dc713bf0e7e67c4b6f6
Moujan Anna <moujan@annawake.com>
2015-03-05 21:17:46 -0800

    OEL 6.x needs /usr/sbin added to the PATH

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -4,4 +4,4 @@ require 'net/ssh'
 set :backend, :ssh
 
 # Set PATH (OEL 5 does not include /sbin by default)
-set :path, '/sbin:/usr/local/sbin:$PATH'
+set :path, '/usr/local/sbin:/usr/sbin:/sbin:$PATH'
