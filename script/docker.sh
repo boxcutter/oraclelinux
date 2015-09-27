b025e688a335bfeb7e4f304954971a5ccc3d6a42
Moujan Anna <moujan@annawake.com>
2015-09-27 09:42:03 -0700

    Add docker images

---
+++ script/docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "==> Run the Docker installation script"
+curl -sSL https://get.docker.com | sh
+
+echo "==> Create the docker group"
+# Add the docker group if it doesn't already exist
+groupadd docker
+
+echo "==> Add the connected "${USER}" to the docker group."
+gpasswd -a ${USER} docker
+gpasswd -a ${SSH_USERNAME} docker
+
+echo "==> Starting docker"
+service docker start
+echo "==> Enabling docker to start on reboot"
+chkconfig docker on
