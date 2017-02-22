bb9d75020e9035a8dd2006fc94e6954e214fd551
Martin Gruner <martin.gruner@otrs.com>
2017-02-22 15:31:24 +0100

    Removed lines duplicated from virtualbox.sh.

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -62,9 +62,6 @@ if [ $(ls | wc -w) -gt 16 ]; then
 fi
 popd
 
-echo "==> Remove packages needed for building guest tools"
-yum -y remove gcc cpp libmpc mpfr kernel-devel kernel-headers perl
-
 echo '==> Clean up yum cache of metadata and packages to save space'
 yum -y clean all
 
